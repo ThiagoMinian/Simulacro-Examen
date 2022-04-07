757ebb5f7680edd12a51dd60a92674112a9f1ec7
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-04-07 13:39:04 -0300

    Puse la consigna del ejercicio 1

--- Assets/Scripts/Ejercicio1.cs
+++ Assets/Scripts/Ejercicio1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
 
 public class Ejercicio1 : MonoBehaviour
 {
